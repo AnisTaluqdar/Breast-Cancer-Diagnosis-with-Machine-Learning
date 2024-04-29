b4c4a242da4d433cb860578c72abb10e5ae1a077
Anis Taluqdar <anistaluqdar@gmail.com>
2024-04-30 01:24:56 +0600

    up

---
+++ main.py
@@ -0,0 +1,47 @@
+# %%
+# Data Exploration
+
+#     Load the dataset and explore its characteristics.
+#     Handle missing values (if any).
+#     Normalize features (e.g., Min-Max scaling or Z-score normalization).
+#     Split the data into training and testing sets.
+
+# %%
+# import necessary libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+from IPython import display
+display.set_matplotlib_formats('svg')
+# %%
+# Load the dataset and explore its characteristics.
+df = pd.read_csv('/home/anis/Documents/Projects/breast_cancer_wisconsin.csv')
+df
+# %%
+# Handle missing values (if any).
+print(df.isnull().sum().sum())
+
+print(df.isnull().sum())
+
+# remove null value
+df.dropna(axis=1, inplace=True)
+
+# check null value again
+print(df.isnull().sum().sum())
+# %%
+# Data description
+df.describe()
+# %%
+# data information
+df.info()
+# %%
+df['diagnosis']
+# %%
+# one-hot encoding
+
+df = pd.get_dummies(df, columns=['diagnosis'], drop_first=True)
+# %%
+df
+# %%
